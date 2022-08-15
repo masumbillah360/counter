d067902b4cab4f6b53ffeb4a39b5eabd5e89ce03
masumbillah360 <masumbillah6584@gmail.com>
2022-08-15 19:17:10 +0600

    added 0 limit on minus btn

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ let counter = 0;
 document.getElementById('btnPlus').addEventListener('click',()=>{
     counter++;
     counterText.innerText = counter;
+    document.getElementById('btnMinus').removeAttribute('disabled',true);
+    
 })
 document.getElementById('btnMinus').addEventListener('click',()=>{
+    
+    if (counterText.innerText == 1) {
+        document.getElementById('btnMinus').setAttribute('disabled',true);
+    }
     counter--;
     counterText.innerText = counter;
 })
